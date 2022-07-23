74a9b780c7e1c0a3aaa6e6523d25acf57367cd2a
Lorenzo Botti <bottilorenzobg@gmail.com>
2022-07-23 07:34:37 +0200

    Silence warning in utils module

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -9,6 +9,7 @@ pub fn take_n<'a>(input: &'a str, n: usize) -> &'a str {
     &input[..bytes]
 }
 
+#[allow(dead_code)]
 pub fn match_start<'a>(input: &'a str, matcher: &str) -> Option<&'a str> {
     if input.starts_with(matcher) {
         Some(&input[..matcher.bytes().len()])
