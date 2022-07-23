9bbcb04c1da0b07d3fe5af8ad3cb1ce921eaaf0b
Lorenzo Botti <bottilorenzobg@gmail.com>
2022-07-23 16:09:05 +0200

    Fix off-by-one in match_str and and tests

--- src/rules/literal.rs
+++ src/rules/literal.rs
@@ -44,14 +44,17 @@ impl<'a> Literal<'a> {
                     }
                 }
 
+                let mut rest = chars.as_str();
                 if repeated {
                     while let Some(rune) = chars.next() {
                         if !in_range(rune, from, to) {
                             break;
+                        } else {
+                            rest = chars.as_str();
                         }
                     }
                 }
-                return Some(trim_end(input, chars.as_str()));
+                return Some(trim_end(input, rest));
             }
             LiteralContent::Str(string) => {
                 let mut left: &'b str = trim_start(input, string)?;
@@ -147,5 +150,54 @@ impl<'a> From<&DoubleQuote<'a>> for RulePieceContent<'a> {
 impl<'a> From<&CharRange> for RulePieceContent<'a> {
     fn from(range: &CharRange) -> Self {
         Self::Literal(range.into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn matches() {
+        let inputs: [(_, Literal, _, _); 6] = [
+            ("oleniso  burger", LiteralContent::Range { from: 'a', to: 'z' }.into(), true, Some("oleniso")),
+            ("AAAAAAAAaaaa", LiteralContent::Range { from: 'A', to: 'Z' }.into(), true, Some("AAAAAAAA")),
+            ("AAAAAAAAaaaa", LiteralContent::Range { from: 'A', to: 'Z' }.into(), false, Some("A")),
+            ("aperol", LiteralContent::Range { from: 'a', to: 'p' }.into(), true, Some("ape")),
+            ("hamburger mobile", LiteralContent::Str("hamburg").into(), false, Some("hamburg")),
+            ("hamburger mobile", LiteralContent::Str("mobile").into(), true, None),
+        ];
+
+        for (input, literal, repeated, expected) in inputs {
+            let got = literal.match_str(input, repeated);
+            assert_eq!(expected, got);
+        }
+    }
+
+    #[test]
+    fn is_in_range() {
+        #[derive(Debug, Copy, Clone)]
+        struct TestCase {
+            from: char,
+            to: char,
+            input: char,
+            expected: bool,
+        }
+
+        let cases = [
+            TestCase{ from: 'a', to: 'z', input: 'b', expected: true },
+            TestCase{ from: 'a', to: 'z', input: 'B', expected: false },
+            TestCase{ from: 'A', to: 'Z', input: 'B', expected: true },
+            TestCase{ from: 'a', to: 'z', input: ' ', expected: false },
+            TestCase{ from: '0', to: '9', input: ' ', expected: false },
+            TestCase{ from: '0', to: '9', input: '3', expected: true },
+            TestCase{ from: '0', to: '9', input: '0', expected: true },
+            TestCase{ from: 'a', to: 'z', input: 'z', expected: true },
+        ];
+
+        for tc in cases {
+            dbg!(tc);
+            assert_eq!(in_range(tc.input, tc.from, tc.to), tc.expected)
+        }
     }
 }
